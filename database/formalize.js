0791544ef1fd1df91e203fc3885fb31ac6a3265a
Евгений Волков <vamper.2012@gmail.com>
2020-01-10 12:26:19 +0800

    Update formalize.js

--- database/formalize.js
+++ database/formalize.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const questionsBank = require("./db-10.01.20.js");
 //let questionsBank = require("./db.1.json");
@@ -8,35 +8,41 @@ console.log(questionsBank.size);
 let index = 0;
 
 for (let i = 0; i < questionsBank.length; i++) {
-    //console.log(i + " -- " + questionsBank[i].text);
-    
-    //console.log(questionsBank[i].text + "\n");
-    questionsBank[i].text = questionsBank[i].text.replace(/\%.*?\%|(\%)|\»|\«|•\s|\&\;|\<|\>|br|&nbsp;|\“|\”|$|\_|\s$|\s\Z|\.$|\:|\;|\.\Z|\—/g, "")
-                                                .replace(/\.$|\.\Z/g, "")
-                                                .replace(/\s\,/g, ",")
-                                                .replace(/\s-\s|\-|\-\s|\n|\\|\//g, " ")
-                                                .replace(/\s\;/g, ";")
-                                                .replace(/\s\./g, ".")
-                                                .replace(/\s\s|\s\s\s|\s\s\s\s|\s\s\s\s\s/g, " ")
-                                                .replace(/\s\s|\s\s\s|\s\s\s\s|\s\s\s\s\s/g, " ");
-                                                
+  //console.log(i + " -- " + questionsBank[i].text);
 
-    for (let j = 0; j < questionsBank[i].answers.length; j++) {   
-        //console.log("\n\t" + j);
-        questionsBank[i].answers[j][0] = questionsBank[i].answers[j][0].replace(/\%.*?\%|(\%)|\(.*?\)|\(|\)|\»|\«|\;|\.|\,\Z|\,$|br|&nbsp;|\“|\”|\•|\.$|\—/g, "")
-                                                                        .replace(/\s\,/g, ",")
-                                                                        .replace(/\s\;/g, ";")
-                                                                        .replace(/\s\./g, ".")
-                                                                        .replace(/\-|\s–\s|\s\s/g, " ")
-                                                                        .replace(/\ё/g, "е");
-                                
-    }  
+  //console.log(questionsBank[i].text + "\n");
+  questionsBank[i].text = questionsBank[i].text
+    .replace(
+      /\%.*?\%|(\%)|\»|\«|•\s|\&\;|\<|\>|br|&nbsp;|\“|\”|$|\_|\s$|\s\Z|\.$|\:|\;|\.\Z|\—/g,
+      ""
+    )
+    .replace(/\.$|\.\Z/g, "")
+    .replace(/\s\,/g, ",")
+    .replace(/\s-\s|\-|\-\s|\n|\\|\//g, " ")
+    .replace(/\s\;/g, ";")
+    .replace(/\s\./g, ".")
+    .replace(/\s\s|\s\s\s|\s\s\s\s|\s\s\s\s\s/g, " ")
+    .replace(/\s\s|\s\s\s|\s\s\s\s|\s\s\s\s\s/g, " ");
 
-    index = i;
+  for (let j = 0; j < questionsBank[i].answers.length; j++) {
+    //console.log("\n\t" + j);
+    questionsBank[i].answers[j][0] = questionsBank[i].answers[j][0]
+      .replace(
+        /\%.*?\%|(\%)|\(.*?\)|\(|\)|\»|\«|\;|\.|\,\Z|\,$|br|&nbsp;|\“|\”|\•|\.$|\—/g,
+        ""
+      )
+      .replace(/\s\,/g, ",")
+      .replace(/\s\;/g, ";")
+      .replace(/\s\./g, ".")
+      .replace(/\-|\s–\s|\s\s/g, " ")
+      .replace(/\ё/g, "е");
+  }
+
+  index = i;
 }
 
 const fileOut = JSON.stringify(questionsBank, null, 4);
 
-fs.writeFileSync('./db.2.json', fileOut);
+fs.writeFileSync("./db.2.json", fileOut);
 
 console.log(index);
